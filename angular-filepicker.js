8a5fc4fad8c41dcc24d4082599f000fbb73c2d29
George <randallmeeker@gmail.com>
2013-10-18 21:16:37 -0600

    Create angular-filepicker.js

---
+++ angular-filepicker.js
@@ -0,0 +1,36 @@
+app.directive("filepicker", function($location){
+	return {
+		scope: {
+			callback: '&',
+			label: '@'
+		},
+		transclude: true,
+		restrict: "A",
+		template: "<a href='' class='btn btn-default' ng-click='pickFiles()' ng-transclude></a>",
+		link: function(scope, element, attrs) {
+			scope.pickFiles = function () {
+			    var picker_options = {
+			        'container': 'modal',
+			        'services': ['BOX','COMPUTER','WEBCAM','DROPBOX','EVERNOTE','FACEBOOK','FLICKR','FTP','GITHUB','GOOGLE_DRIVE','SKYDRIVE','PICASA','WEBDAV']
+			    };
+
+			    if(attrs.multiple == 'true'){
+			    	picker_options['multiple'] = true;
+			    }
+
+			    var path = attrs.path ? attrs.path : '/uploads/';
+
+			    var store_options = {
+			    	location: 'S3',
+			    	path: path
+			    }
+
+			    filepicker.pickAndStore(picker_options, store_options, function (fpfiles) {
+			        scope.$apply(function(){
+			        	scope.callback({file:fpfiles});
+			        });
+			    });
+			};
+		}
+	}
+})
