fecab574d53724d66bb30cb5f114d77483906ef5
Randall Meeker <randallmeeker@gmail.com>
2014-03-05 17:27:42 -0600

    Update angular-filepicker.js
    added pickerclass to set the class on the a tag, added mimetypes and multiple

--- angular-filepicker.js
+++ angular-filepicker.js
@@ -1,35 +1,35 @@
-app.directive("filepicker", function($location){
+app.directive("filepicker", function(){
 	return {
 		scope: {
-			callback: '&'
+			callback: '&',
+			'pickerclass': '@'
 		},
 		transclude: true,
 		restrict: "A",
-		template: "<a href='' class='btn btn-default' ng-click='pickFiles()' ng-transclude></a>",
+		template: "<a href='javascript:;' class='{{pickerclass}}' ng-click='pickFiles()' ng-transclude></a>",
 		link: function(scope, element, attrs) {
 			scope.pickFiles = function () {
-			    var picker_options = {
-			        'container': 'modal',
-			        'services': ['BOX','COMPUTER','WEBCAM','DROPBOX','EVERNOTE','FACEBOOK','FLICKR','FTP','GITHUB','GOOGLE_DRIVE','SKYDRIVE','PICASA','WEBDAV']
-			    };
+				var picker_options = {
+					container: 'modal',
+					mimetypes: attrs.mimetypes ? eval(attrs.mimetypes) : ['*'],
+					multiple: attrs.multiple ? eval(attrs.multiple) : false
+					};
 
-			    if(attrs.multiple == 'true'){
-			    	picker_options['multiple'] = true;
-			    }
+				var path = attrs.path ? attrs.path : '/uploads/',
+					container = attrs.container ? attrs.container : 'documents.e-freightliner.com';
 
-			    var path = attrs.path ? attrs.path : '/uploads/';
+				var store_options = {
+					location: 'S3',
+					path: path,
+					container: container
+				};
 
-			    var store_options = {
-			    	location: 'S3',
-			    	path: path
-			    }
-
-			    filepicker.pickAndStore(picker_options, store_options, function (fpfiles) {
-			        scope.$apply(function(){
-			        	scope.callback({file:fpfiles});
-			        });
-			    });
+				filepicker.pickAndStore(picker_options, store_options, function (fpfiles) {
+					scope.$apply(function(){
+						scope.callback({file:fpfiles});
+					});
+				});
 			};
 		}
-	}
-})
+	};
+});
